fdcdd7e3dfb27b65e432535f5c779c40f66e8071
gaurav nasa <gauravnasafe@gmail.com>
2022-09-04 10:09:32 +0530

    header css added and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+      <p>Copy Rights &copy; Gaurav Nasa 2022</p>
+      <p>All Products displayed in this site are for demo purpose please dont claim any payback or refund amount .</p>
+    </footer>
   )
 }
