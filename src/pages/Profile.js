edede4c2d26b9c939284ebc408f284934842b570
gaurav nasa <gauravnasafe@gmail.com>
2022-09-04 10:59:03 +0530

    added proile and css

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 
 export default function Profile() {
+  const state = useSelector((s) => s);
+  const { user } = state;
+  const [p, setp] = useState(false);
+  const hc = () => setp(!p);
   return (
-    <div>Profile</div>
-  )
+    <div className="profile">
+      <div>
+        <h1>my Profile</h1>
+        <p>name : {user?.name}</p>
+        <p>email : {user?.email}</p>
+        <p>phone : {user?.phone}</p>
+        <p>
+          password {"  "}
+          {!p ? (
+            <i onClick={hc} className="fa fa-eye"></i>
+          ) : (
+            <i onClick={hc} className="fa fa-lock"></i>
+          )}{" "}
+          : {p ? user?.password : "***"}
+        </p>
+      </div>
+    </div>
+  );
 }
