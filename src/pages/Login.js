27fa8034ab3bd719c7b17cb6350caa249e092435
gaurav nasa <gauravnasafe@gmail.com>
2022-09-04 10:20:20 +0530

    auth css

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "react-hook-form";
 
 export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
   return (
-    <div>Login</div>
-  )
+    <form className="auth" onSubmit={handleSubmit(onSubmit)}>
+      <h1>Login</h1>
+      <div>
+        <p>email</p>
+        <input placeholder="email" {...register("email", { required: true })} />
+        {errors.email && <p className="error">username is required</p>}
+      </div>
+
+      <div>
+        <p>password</p>
+        <input
+          placeholder="password"
+          {...register("password", { required: true })}
+        />
+        {errors.password && <p className="error">password is required</p>}
+      </div>
+
+      <button>login</button>
+    </form>
+  );
 }
